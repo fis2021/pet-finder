776691ec9934e42bf1ed0957b229ca0e9a2b254f
Stefan Chivu <stefan.chivu1@yahoo.com>
2021-04-13 11:39:31 +0300

    PET-50 Adapted UserAlreadyExistsException

--- src/main/java/org/exceptions/UsernameAlreadyExistsException.java
+++ src/main/java/org/exceptions/UsernameAlreadyExistsException.java
@@ -2,6 +2,10 @@ package org.exceptions;
 
 public class UsernameAlreadyExistsException extends Exception {
 
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
     private String username;
 
     public UsernameAlreadyExistsException(String username) {
