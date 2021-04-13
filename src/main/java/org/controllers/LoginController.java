54b5066df452b9b185aa73702a59325cea4b6d0e
Stefan Chivu <stefan.chivu1@yahoo.com>
2021-04-13 11:42:04 +0300

    PET-43 Implemented redirect to register logic

--- src/main/java/org/controllers/LoginController.java
+++ src/main/java/org/controllers/LoginController.java
@@ -1,12 +1,20 @@
 package org.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import org.exceptions.UserDoesNotExistException;
+import org.model.User;
 import org.services.UserService;
+
 
 public class LoginController {
 
@@ -19,14 +27,23 @@ public class LoginController {
     @FXML
     private String role;
 
-
     @FXML
-    public void handleRegisterAction() {
+    public void handleLoginAction() {
         try {
-            UserService.login(usernameField.getText(), passwordField.getText());
+            User user = UserService.login(usernameField.getText(), passwordField.getText());
             loginMessage.setText("Logged-In succesfully");
         } catch (UserDoesNotExistException e) {
             loginMessage.setText(e.getMessage());
         }
+    }
+
+    @FXML
+    public void redirectToRegister(ActionEvent event) throws Exception{
+        Node node = (Node) event.getSource();
+        Stage currentStage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
+        currentStage.setTitle("Register");
+        currentStage.setScene(new Scene(root, 500, 500));
+        currentStage.show();
     }
 }
