40512a61361f53e58c6549323b162c6232d0a17a
Stefan Chivu <stefan.chivu1@yahoo.com>
2021-04-16 00:11:51 +0300

    Modified branch name

--- src/main/java/org/controllers/RegistrationController.java
+++ src/main/java/org/controllers/RegistrationController.java
@@ -1,10 +1,20 @@
 package org.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import org.exceptions.UsernameAlreadyExistsException;
 import org.services.UserService;
 
@@ -25,18 +35,26 @@ public class RegistrationController {
     }
 
     @FXML
-    public void handleRegisterAction() {
+    public void handleRegisterAction(ActionEvent event) throws IOException, InterruptedException {
         try {
             if(usernameField.getText() == "" || passwordField.getText() == "" || role.getValue() == ""){
                 throw new NullPointerException("Username and password required");
             }
-            
+
             UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue());
-            registrationMessage.setText("Account created successfully!");
+            registrationMessage.setText("Account created successfully!\nRedirecting to login...");
+
         } catch (UsernameAlreadyExistsException e) {
             registrationMessage.setText(e.getMessage());
         } catch(Exception e){
             registrationMessage.setText(e.getMessage());
         }
+            TimeUnit.SECONDS.sleep(2);
+            Node node = (Node) event.getSource();
+            Stage currentStage = (Stage) node.getScene().getWindow();
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+            currentStage.setTitle("Register");
+            currentStage.setScene(new Scene(root, 500, 500));
+            currentStage.show();
     }
 }
