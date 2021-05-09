35b0bd88b073bc3e9f9515df6358d38de5606ffb
Stefan Chivu <stefan.chivu1@yahoo.com>
2021-05-09 20:20:51 +0300

    PET-98 Modified Individual Homepage for requests

--- src/main/java/org/model/ImageStringTableRow.java
+++ src/main/java/org/model/ImageStringTableRow.java
@@ -1,13 +1,36 @@
 package org.model;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.image.ImageView;
 
 public class ImageStringTableRow {
     private ImageView imageView;
-    private String info;
+    //private final SimpleStringProperty info;
+    private final String info;
 
     public ImageStringTableRow(ImageView imageView, String info){
         this.imageView = imageView;
+        //this.info = new SimpleStringProperty(info);
         this.info = info;
     }
+
+    public final String getInfo(){
+        return info;
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public void setImageView(ImageView imageView){
+        this.imageView = imageView;
+    }
+
+    // SimpleStringProperty getInfo(){
+    //    return this.info;
+    //}
+
+    //public void setInfo(String info){
+    //    this.info.set(info);
+    //}
 }
