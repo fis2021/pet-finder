345a004e9b2c489185eb966c0c6ad98789df52a3
Alina <butoidraghicialina@gmail.com>
2021-05-10 23:29:58 +0300

    PET-108 UI test for Login

---
+++ src/test/java/org/LoginTest.java
@@ -0,0 +1,56 @@
+package org;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.services.DatabaseService;
+import org.services.FileSystemService;
+import org.services.UserService;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.testfx.assertions.api.Assertions.assertThat;
+
+@ExtendWith(ApplicationExtension.class)
+class LoginTest {
+
+    public static final String USER_1 = "user1";
+    public static final String PASSWORD_1 = "password1";
+
+    @BeforeEach
+    void setUp() throws Exception {
+        FileSystemService.APPLICATION_FOLDER = ".test-registration-example";
+        FileUtils.cleanDirectory(FileSystemService.getApplicationHomeFolder().toFile());
+        DatabaseService.initDatabase();
+    }
+
+    @Start
+    void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+        primaryStage.setTitle("Petfinder");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    @Test
+    void testLogin(FxRobot robot) {
+        robot.clickOn("#username");
+        robot.write(USER_1);
+        robot.clickOn("#password");
+        robot.write(PASSWORD_1);
+        robot.clickOn("#loginButton");
+
+        assertThat(robot.lookup("#loginMessage").queryText()).hasText("Invalid username/password");
+        assertThat(UserService.getAllUsers()).size().isEqualTo(0);
+
+        robot.clickOn("#redirectToRegisterButton");
+
+    }
+}
